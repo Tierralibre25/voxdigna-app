371aa1077f1b8199e542b9ddb70cc9004baa9ec1
Tierralibre25 <info@cooperativatierralibre.es>
2025-07-29 12:27:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  // Build settings
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      // Punto di ingresso: il tuo index in root
-      input: path.resolve(__dirname, 'index.html'),
-    }
-  },
-  // Assicuriamoci che public/ sia solo asset
   publicDir: 'public',
+  resolve: {
+    alias: {
+      '~/': `${__dirname}/src/`
+    }
+  }
 })
